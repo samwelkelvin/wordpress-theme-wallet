525866194f4da0676db4bb412d5d00816aa1bd95
SAM <samwelmunyaka@gmail.com>
2024-11-14 16:32:37 +0300

    Fixed issues

--- single.php
+++ single.php
@@ -1,23 +1,27 @@
 <?php
 
 /**
- * Didplays single blog post.
+ * Displays single blog post.
  */
 try {
 	get_header();
+
 	$args = array();
 
 	if (have_posts()) {
 		while (have_posts()) {
 			the_post();
-			echo 1;
+
 			get_template_part('template-parts/content/single', 'post', $args);
 		}
 	} else {
-		echo '<h3 class"text-center">Oops ! No data.</h3>';
+		echo '<h3 class="text-center">Oops ! No data.</h3>';
 	}
 
 	get_footer();
 } catch (Throwable $th) {
-	echo $th->getMessage();
+	// Log the error instead of echoing it directly
+	error_log($th->getMessage());
+
+	echo '<h3 class="text-center">An error occurred.</h3>';
 }
