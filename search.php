de6dda8eb32efb2aa33b1b78de7cf95ee0ac931c
SAM <samwelmunyaka@gmail.com>
2024-11-14 16:26:18 +0300

    Updated

--- search.php
+++ search.php
@@ -1,9 +1,5 @@
 <?php
-/*
-*
-*  Displays all search results
-*
-*/
+// Displays all search results
 
 ?>
 <style>
@@ -15,40 +11,26 @@
 <?php
 
 try {
-
-    get_header();
-
-?>
+	get_header();
+	?>
 
     <div class="container">
-
         <h3 class="h3 mb-4  py-3">
             Search results for : <mark> <?php echo esc_html(get_search_query()); ?></mark>
         </h3>
     </div>
 
-    <?php
+    <?php $args = array();
 
-    $args = [];
-
-    //if blog layout is list or not set use list
-    if (!get_theme_mod('blog-layout') || get_theme_mod('blog-layout') === 'list')
-
-        get_template_part('template-parts/content/list', 'posts-template', $args);
-
-    else
-
-        get_template_part('template-parts/content/grid', 'posts-template', $args);
-
-
-    ?>
-
+	// if blog layout is list or not set use list
+	if (!get_theme_mod('blog-layout') || 'list' === get_theme_mod('blog-layout')) {
+		get_template_part('template-parts/content/list', 'posts-template', $args);
+	} else {
+		get_template_part('template-parts/content/grid', 'posts-template', $args);
+	}
+	?>
 <?php
-
-    get_footer();
-    
-} catch (\Throwable $th) {
-
-    echo $th->getMessage();
-
+	get_footer();
+} catch (Throwable $th) {
+	echo $th->getMessage();
 }
