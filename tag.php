c47cc5a9fc06c828646a606578d94ac25d9c2b1a
SAM <samwelmunyaka@gmail.com>
2024-11-14 15:19:21 +0300

    Upadated

--- tag.php
+++ tag.php
@@ -1,12 +1,8 @@
 <?php
 
 /**
- * 
- * Didplays  posts based on tag
- * 
+ * Didplays  posts based on tag.
  */
-
-
 ?>
 <style>
     .section {
@@ -17,40 +13,33 @@
 <?php
 
 try {
+	get_header();
 
-    get_header();
-
-?>
+	?>
 
     <div class="container">
 
         <h3 class="h3 mb-4  py-3">
-            Posts with : <mark> <?php echo esc_html(single_tag_title('', false)) ?></mark> tag
+            Posts with : <mark> <?php echo esc_html(single_tag_title('', false)); ?></mark> tag
         </h3>
     </div>
 
     <?php
 
-    $args = [];
+		$args = array();
 
-    //if blog layout is list or not set use list
-    if (!get_theme_mod('blog-layout') || get_theme_mod('blog-layout') === 'list')
+	// if blog layout is list or not set use list
+	if (!get_theme_mod('blog-layout') || 'list' === get_theme_mod('blog-layout')) {
+		get_template_part('template-parts/content/list', 'posts-template', $args);
+	} else {
+		get_template_part('template-parts/content/grid', 'posts-template', $args);
+	}
 
-        get_template_part('template-parts/content/list', 'posts-template', $args);
-
-    else
-
-        get_template_part('template-parts/content/grid', 'posts-template', $args);
-
-
-    ?>
+	?>
 
 <?php
 
-    get_footer();
-
-} catch (\Throwable $th) {
-
-    echo $th->getMessage();
-
+	get_footer();
+} catch (Throwable $th) {
+	echo $th->getMessage();
 }
